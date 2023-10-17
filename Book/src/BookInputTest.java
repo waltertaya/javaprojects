a920fa34e6099c0d8fd11a4e963849c0cd7ee2f4
waltertaya <waltermittybecker5@gmail.com>
2023-10-17 17:46:29 +0300

    classes in Java

---
+++ Book/src/BookInputTest.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class BookInputTest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Book BookTest = new Book();
+        System.out.println("Please enter the title of the book");
+        BookTest.title = input.nextLine();
+        System.out.println("Please enter the title of the book");
+        BookTest.author = input.nextLine();
+        System.out.println("Please enter the title of the book");
+        BookTest.numberOfPages = input.nextInt();
+        System.out.println("The book title is: " + BookTest.title);
+        System.out.println("The book author is: " + BookTest.author);
+        System.out.println("The book has: " + BookTest.numberOfPages + " pages");
+    }
+}
